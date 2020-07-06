20aee8c8ca81bf18892d8349ca0324a32c0a00e2
CheaC <cheacliatt@gmail.com>
2020-07-06 19:53:55 -0400

    It's sort of working. Parsed the data, array starts at 0 for id

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // 1. Require express
 const express = require("express");
 const path = require("path");
-const db = require("./db/db.json");
+let db = require("./db/db.json");
 const fs = require("fs");
 // 2. create an instance of express called app
 const app = express();
@@ -11,44 +11,52 @@ const PORT = process.env.PORT || 3000;
 // 5. Get these bad boys from processing POST routes
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
-
-
+app.use(express.static("public"));
 
 // View / HTML
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
-
 
 // API / JSON
 
-app.get("/api/notes", function(req, res) {
-    return res.json(db);
+app.get("/api/notes", function (req, res) {
+    res.json(db);
 });
 
-app.post("/api/notes", function(req, res) {
-    const newNote = req.body;
+app.post("/api/notes", function (req, res) {
+  fs.readFile("./db/db.json", function (err, data) {
+    if (err) {
+      console.log(err);
+    }
+
+
+    db = JSON.parse(data);
+    const newNote = { ...req.body, id: db.length };
     console.log(newNote);
     db.push(newNote);
 
-    fs.writeFile("./db/db.json", JSON.stringify(db), function (err){
-        if(err){
-            console.log(err);
-        } res.json(newNote);
+    fs.writeFile("./db/db.json", JSON.stringify(db), function (err) {
+      if (err) {
+        console.log(err);
+      }
+      res.json(db);
     });
 
-    
-    
+
+
+  });
 });
 
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 // 4. listen on that port
 app.listen(PORT, (req, res) => {
-    console.log(`Currently running on http://localhost:${PORT}`);
+  console.log(`Currently running on http://localhost:${PORT}`);
 });
